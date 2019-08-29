dc14c2932e7cce10bbda8f2d2c3a25f8ccfa439b
Dave Niewinski <dniewinski@clearpathrobotics.com>
2019-08-29 09:58:28 -0400

    Updated iiwa description for standard mounting configuration.  Added moveit config (untested)

---
+++ boxer_iiwa_moveit_config/scripts/customize_moveit.sh
@@ -0,0 +1,15 @@
+if [ $# == 1 ]
+then
+  echo "Creating Directory <" $PWD"/"$1 ">"
+  mkdir "$1"
+  cd "$1"
+
+  echo "Copying IIWA Moveit Config"
+  cp -r $(catkin_find boxer_iiwa_moveit_config)/. .
+  echo "Updating Package"
+  grep -rli 'boxer_iiwa_moveit_config' * | xargs -i@ sed -i 's/boxer_iiwa_moveit_config/'$1'/g' @
+  echo "Done"
+
+else
+  echo "USAGE: customize_moveit.sh [new_moveit_package_name]"
+fi
